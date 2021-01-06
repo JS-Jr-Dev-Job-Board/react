430ce117f3b8a49f6cb37f624469214ee852a7e8
willwearing <willwearing@gmail.com>
2021-01-06 11:44:08 -0700

    have realised we can't start due to grommet theme not being complete

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <p>https://github.com/grommet/design-kit</p>
+      <p>
+        https://www.figma.com/file/Zs9FqplExrM5sgCsqE9p1Z/DevJobs-1?node-id=0%3A1
+      </p>
+      <p>https://github.com/grommet/grommet-starter-new-app</p>
+    </div>
+  );
+};
+
+export default Portfolio;
