639f1e8d798e7038883a84f7fd8ae82801faabf8
Fatima Rizvi <fatimarzv23@gmail.com>
2021-01-07 18:09:13 -0600

    Slightly built out the jsx

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,22 +8,39 @@ import {
   } from 'recoil';
 
 //  Create a state to hold everyone on the team. For now we'll pass in an object, later we'll fetch it from an api first
-const teamState = atom({
-    key: 'teamState',
+const teamMemberState = atom({
+    key: 'teamMemberState',
     default: [
         {
-            
+            name: "Simone Ballard",
+            pic: "src\img\simone-ballardPNG.png",
+            description: "Lorem ipsum"
+        },
+        {
+            name: "Aszalea Calderon",
+            pic: "src\img\aszelea-calderonPNG.png",
+            description: "Lorem ipsum"
+        },
+        {
+            name: "Mike Hill",
+            pic: "src\img\mike-hillPNG.png",
+            description: "Lorem ipsum"
         }
-    ]
-})
+    ],
+});
 
 function AboutUs() {
+    const teamMembers = useRecoilValue(teamMemberState);
 
     return (
         <div className = "About-Us">
             {/* We might need to move the RecoilRoot bit into the index folder, as it's suppsed to be in the root component. That's easy to change, but I didn't want to possibly conflict with any of the other branches out right now */}
             <RecoilRoot>    
-
+                <h1>About Us</h1>
+                <h4>A blurb</h4>
+                {teamMembers.map((teamMember) => (
+                    <TeamMember key = {teamMember.id} member = {teamMember} />
+                ))}
             </RecoilRoot>
         </div>
     )
