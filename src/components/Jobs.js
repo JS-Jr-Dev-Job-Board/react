baab16c2ea2f55fe9aa315c55fe8795b8e64bc37
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2021-01-10 16:45:33 -0500

    jobs rendering with recoil

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,8 +1,55 @@
 import React from "react";
+import JobCard from "./jobcard"
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+
+const jobCardState = atom({
+  key: "jobCardState",
+  default: [
+    {
+      companyName: "Company 1",
+      companyInfo: "Info about job. It's so cool!",
+      aboutJob: "About this job",
+      technologies: "reactjs",
+      jobDescription:
+        " Sloop pink chase guns list rigging hulk swing the lead Letter of Marque grog blossom",
+      skills: "react",
+    },
+    {
+        companyName: "Company 2",
+        companyInfo: "Info about job. It's so cool!",
+        aboutJob: "About this job",
+        technologies: "reactjs",
+        jobDescription:
+          " Sloop pink chase guns list rigging hulk swing the lead Letter of Marque grog blossom",
+        skills: "react",
+
+    },
+    {
+        companyName: "Company 1",
+        companyInfo: "Info about job. It's so cool!",
+        aboutJob: "About this job",
+        technologies: "reactjs",
+        jobDescription:
+          " Sloop pink chase guns list rigging hulk swing the lead Letter of Marque grog blossom",
+        skills: "react",
+    }
+  ],
+});
 
 const Jobs = () => {
+  const jobCards = useRecoilValue(jobCardState);
   return (
     <div>
+               {jobCards.map((jobCard) => (
+        <JobCard key={jobCard.id} card={jobCard} />
+      ))}
+   
       <p>This is a jobs page</p>
     </div>
   );
