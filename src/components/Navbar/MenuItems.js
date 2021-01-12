adb8b36475d4c91d55924a35acc10ab0e31513f5
willwearing <willwearing@gmail.com>
2021-01-11 20:18:14 -0700

    finished navbar - it needs to be integrated

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Jobs",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Profile",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign up",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { Button } from "./Button/Button";
+import { MenuItems } from "./MenuItems";
+
+const Navbar = () => {
+  const [on, setOpen] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setOpen(!on);
+  };
+
+  return (
+    <div>
+      <nav className="NavbarItems">
+        <h1 className="NavbarLogo">
+          React<i className="fab fa-react"></i>
+        </h1>
+        <div className="menu-icon" onClick={onSubmit}>
+          <i className={on ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={on ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button>Sign up</Button>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
