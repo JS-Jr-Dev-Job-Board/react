d50988d515f2d31e6bcf53c52301c0174b161afa
Aszalea Calderon <aszalea_calderon@yahoo.com>
2021-01-13 10:54:01 -0800

    starting the styling for signin

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,5 +1,14 @@
+import axios from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useState } from "react/cjs/react.development";
+import styled from "styled-components";
+
+//STYLE
+
+const SignInH1 = styled.h1`
+  color: ${(pr) => pr.theme.primaryColor};
+`;
 
 const Signup = () => {
   //STATE
@@ -7,22 +16,32 @@ const Signup = () => {
 
   //ONSUBMIT
   const onSubmit = (data) => {
+    axios
+      .post("#")
+      .then((res) => {
+        debugger;
+      })
+      .catch((res) => {
+        debugger;
+      });
     console.log(data);
   };
 
   //JSX- ref={register} handles and tracks changes and prevents the need to track value and onChanges manually
+
   return (
     <>
       <div>
         <div>Image</div>
-        <h1>Sign up</h1>
+        <SignInH1>Sign up</SignInH1>
         <form onSubmit={handleSubmit(onSubmit)}>
           <label>
             Email
             <input
               ref={register({
                 required: "Email is required",
-                minLength: { value: 3, message: "Email is too Short" },
+                minLength: { value: 6, message: "Email is too Short" },
+                shouldDirty: true,
               })}
               type="text"
               placeholder="Email"
@@ -37,7 +56,7 @@ const Signup = () => {
             <input
               ref={register({
                 required: "Password is required",
-                minLength: { value: 8, message: "Password is too Short" },
+                minLength: { value: 12, message: "Password is too Short" },
               })}
               type="password"
               placeholder="Password"
@@ -45,13 +64,16 @@ const Signup = () => {
             />
           </label>
           {errors.password && <p>{errors.password.message}</p>}
-
+          {/* Button Disabled is not needed as it doesn't allow anything to be sent without having the correct amount of info attached.  */}
           <button>Submit</button>
           <br />
-          <a href="#">Have an account? Sign in</a>
+          <a href="#">Don't have an account? Sign up</a>
         </form>
       </div>
     </>
   );
 };
 export default Signup;
+
+//ReactHookButton form reinable.
+//
