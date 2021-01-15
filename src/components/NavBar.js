3aff54998d2fbc5940503b0b3bd77057e67548cb
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2021-01-15 03:03:27 -0500

    imported grommet so it would render in jobs card and melissa's sign up

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,48 +1,54 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-// import components here:
-import AboutUs from "./AboutUs";
-import Jobs from "./Jobs";
-import Portfolio from "./Portfolio";
-import Home from "./Home";
+// THIS IS NOT CURRENTLY IN USE USING OTHER NAVBAR UNDER THE FOLDER NAVBAR
 
-const NavBar = () => {
-  return (
-    <div>
-      <Router>
-        <nav>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/jobs">Jobs</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portfolio</Link>
-          </li>
-        </nav>
 
-        <Switch>
-          <Route path="/about">
-            <AboutUs />
-          </Route>
-          <Route path="/jobs">
-            <Jobs />
-          </Route>
-          <Route path="/portfolio">
-            <Portfolio />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
-};
+// import React from "react";
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default NavBar;
+// // import components here:
+// import AboutUs from "./AboutUs";
+// import Jobs from "./Jobs";
+// import Portfolio from "./Portfolio";
+// import Home from "./Home";
+
+// // look at using this tutorial to make this work properly: https://www.youtube.com/watch?v=fL8cFqhTHwA
+
+// const NavBar = () => {
+//   return (
+//     <div>
+//       <Router>
+//         <nav>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/about">About</Link>
+//           </li>
+//           <li>
+//             <Link to="/jobs">Jobs</Link>
+//           </li>
+//           <li>
+//             <Link to="/portfolio">Portfolio</Link>
+//           </li>
+//         </nav>
+
+//         <Switch>
+//           <Route path="/about">
+//             <AboutUs />
+//           </Route>
+//           <Route path="/jobs">
+//             <Jobs />
+//           </Route>
+//           <Route path="/portfolio">
+//             <Portfolio />
+//           </Route>
+//           <Route path="/">
+//             <Home />
+//           </Route>
+//         </Switch>
+//       </Router>
+//     </div>
+//   );
+// };
+
+// export default NavBar;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button/Button";
+import { MenuItems } from "./MenuItems";
+import AboutUs from "../AboutUs";
+import Jobs from "../Jobs";
+import Portfolio from "../Portfolio";
+import Home from "../Home";
+
+
+const Navbar = () => {
+  const [on, setOpen] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setOpen(!on);
+  };
+
+  return (
+    <div>
+      <Router>
+      <nav className="NavbarItems">
+        <h1 className="NavbarLogo">
+          Jr Dev Jobs<i className="fab fa-react"></i>
+        </h1>
+        <div className="menu-icon" onClick={onSubmit}>
+          <i className={on ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className={on ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button>Sign up</Button>
+      </nav>
+
+      <Switch>
+        <Route path="/about">
+          <AboutUs />
+        </Route>
+        <Route path="/jobs">
+          <Jobs />
+        </Route>
+        <Route path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      </Router>
+    </div >
+  );
+};
+
+export default Navbar;
