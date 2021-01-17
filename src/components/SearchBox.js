626e25d15910485b0010ad4f3d1730a0b599038d
BrianReisman <brianreisman@protonmail.com>
2021-01-17 18:44:28 -0500

    Final commit. Search icon added and textInput styles.

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,60 +1,21 @@
 import React, { useState } from "react";
-import { Box, Form, FormField, TextInput, Grommet } from "grommet";
+import { Box, Form, TextInput, Grommet } from "grommet";
+import { Search } from "grommet-icons";
 import { grommet } from "grommet/themes";
-import { deepMerge } from "grommet/utils";
-import { useRecoilState } from 'recoil';
+import { useRecoilState } from "recoil";
 import { mainSearchQuery } from "../store/atom";
 
-const allJobs = [
-  {
-    Company: "A",
-    Salary: "77,000",
-    Title: "Jr. JS Dev",
-    Location: "Remote",
-  },
-  {
-    Company: "B",
-    Salary: "55,000",
-    Title: "Intern",
-    Location: "OKC",
-  },
-  {
-    Company: "C",
-    Salary: "66,000",
-    Title: "Front-End React Dev",
-    Location: "Miami, FL",
-  },
-  {
-    Company: "D",
-    Salary: "88,000",
-    Title: "Jr/Mid Jr Dev",
-    Location: "Bar Harbor, ME",
-  },
-];
-
-const customTheme = deepMerge(grommet, {
-  global: {
-    colors: {
-      "asdf": {
-        light: "tomato",
-        dark: "blue",
-      },
-      size: {
-        'full': '50%'
-      }
-    },
-  },
-  formField: {
-    border:{
-      color: 'asdf'
-    }
-  },
-});
+// Optional props for this component are:
+// 1. 'placeholder', which sets placehold text in the search field and defaults to 'Search' and
+// 2. 'name', which sets the value on the TextInput's name and id attributes and defaults to 'searchBox'.
 
 const SearchBox = (props) => {
-  const [searchQuery, setSearchQuery] = useRecoilState(mainSearchQuery)
+  const [searchQuery, setSearchQuery] = useRecoilState(mainSearchQuery);
   const [searchInput, setSearchInput] = useState(searchQuery);
-  const searchInfo = props.name ? props.name : "searchBox";
+
+  //All optional props used in this component.
+  const searchInfo = props.name || "searchBox";
+  const addPlaceholder = props.placeholder || "Search";
 
   const onChange = (e) => {
     setSearchInput(e.target.value);
@@ -63,27 +24,32 @@ const SearchBox = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     setSearchQuery(searchInput);
-    };
+  };
 
   return (
-    <Grommet themeMode='dark' theme={customTheme}>
-      <Box width="50%" alignSelf='center'  >
-        <Form onSubmit={onSubmit}>
-          <FormField label="Search" htmlFor={searchInfo}>
-            <TextInput
-              size="small"
-              width="small"
-              a11yTitle="Search Box Text Field"
-              name={searchInfo}
-              id={searchInfo}
-              type="text"
-              placeholder={props.placeholder || "Search"}
-              value={searchInput}
-              onChange={onChange}
-            />
+    <Grommet themeMode="dark" theme={grommet}>
+      <Box>
+        <Box width="25%" alignSelf="end" margin="10px">
+          <Form onSubmit={onSubmit}>
+            <Box direction="row" align="center">
+              <Box margin="10px">
+                <Search />
+              </Box>
+              <TextInput
+                size="small"
+                width="small"
+                a11yTitle="Search Box Text Field"
+                name={searchInfo}
+                id={searchInfo}
+                type="search"
+                placeholder={addPlaceholder}
+                value={searchInput}
+                onChange={onChange}
+              />
+            </Box>
             {searchQuery && <h2>Your search is: {searchQuery}</h2>}
-          </FormField>
-        </Form>
+          </Form>
+        </Box>
       </Box>
     </Grommet>
   );
