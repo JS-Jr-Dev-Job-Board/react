2eecbe3a0b3b64059721862b285b71feabf718ed
BrianReisman <brianreisman@protonmail.com>
2021-04-19 20:53:11 -0400

    footer component rendering to the screen. Three tests written to confirm, all passing

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import StyledFooter from './StyledFooter'
+
+const Footer = (props) => {
+  return (
+    <StyledFooter>
+      <p>Code Shock</p>
+      <p>Career Shock</p>
+      <p>©2021</p>
+    </StyledFooter>
+  )
+}
+
+export default Footer
