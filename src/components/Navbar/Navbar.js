32a6e671a9bddb0777c2c643aa269076acc1cb94
BrianReisman <brianreisman@protonmail.com>
2021-04-20 11:39:52 -0400

    moved Switch + Routes to App.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../../css/Navbar.css'
 import { Button } from './Button/Button'
 import { MenuItems } from './MenuItems'
-import AboutUs from '../AboutUs'
-import Jobs from '../Jobs/Jobs'
-import Portfolio from '../Portfolio'
-import DevDashboard from '../devdash/DevDashboard'
-import Home from '../Home'
 
 const Navbar = () => {
   const [on, setOpen] = useState(false)
@@ -19,46 +14,26 @@ const Navbar = () => {
 
   return (
     <div>
-      <Router>
-        <nav className='NavbarItems'>
-          <h1 className='NavbarLogo'>
-            Jr Dev Jobs<i className='fab fa-react'></i>
-          </h1>
-          <div className='menu-icon' onClick={onSubmit}>
-            <i className={on ? 'fas fa-times' : 'fas fa-bars'}></i>
-          </div>
-          <ul className={on ? 'nav-menu active' : 'nav-menu'}>
-            {MenuItems.map((item, index) => {
-              return (
-                <li key={index}>
-                  <a className={item.cName} href={item.url}>
-                    {item.title}
-                  </a>
-                </li>
-              )
-            })}
-          </ul>
-          <Button>Sign up</Button>
-        </nav>
-
-        <Switch>
-          <Route path='/about'>
-            <AboutUs />
-          </Route>
-          <Route path='/jobs'>
-            <Jobs />
-          </Route>
-          <Route path='/portfolio'>
-            <Portfolio />
-          </Route>
-          <Route path='/dev-dash'>
-            <DevDashboard />  
-          </Route>
-          <Route path='/'>
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <nav className='NavbarItems'>
+        <h1 className='NavbarLogo'>
+          Jr Dev Jobs<i className='fab fa-react'></i>
+        </h1>
+        <div className='menu-icon' onClick={onSubmit}>
+          <i className={on ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+        <ul className={on ? 'nav-menu active' : 'nav-menu'}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+        <Button>Sign up</Button>
+      </nav>
     </div>
   )
 }
