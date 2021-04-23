29fb3c209a632f00e20537ae85d10dfff016c296
unknown <fullstack.melissa@gmail.com>
2021-04-22 18:19:24 -0700

    Footer Added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,115 @@
+import React from 'react'
+// REACT ICONS
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube, FaLinkedin } from 'react-icons/fa'
+
+// COMPONENT'S ELEMENTS
+import {
+    FooterContainer,
+    FooterSubscription,
+    FooterSubHeading,
+    FooterSubText,
+    Form,
+    FormInput,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinksItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    SocialIcon,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink
+} from './Footer.elements'
+
+// GLOBAL STYLES
+import { Button } from '../../globalStyles'
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterSubscription>
+                <FooterSubHeading>
+                    Join our exclusive membership to receive the latest news and trends.
+                </FooterSubHeading>
+                 <FooterSubText>
+                     You can unsubscribe at any time.
+                 </FooterSubText>
+                   <Form>
+                       <FormInput name="email" type="email" placeholder="Your Email" />
+                           <Button fontBig>Subscribe</Button> 
+                   </Form>
+            </FooterSubscription>
+            <FooterLinksContainer>
+                <FooterLinksWrapper>
+                    <FooterLinksItems>
+                        <FooterLinkTitle>
+                            About Us
+                        </FooterLinkTitle>
+                        <FooterLink to="/sign-up">How it works</FooterLink>
+                        <FooterLink to="/">Testimonials</FooterLink>
+                        <FooterLink to="/">Career</FooterLink>
+                        <FooterLink to="/">Investors</FooterLink> 
+                        <FooterLink to="/">Terms of Service</FooterLink>
+                    </FooterLinksItems>
+
+                    <FooterLinksItems>
+                        <FooterLinkTitle>
+                            Contact Us
+                        </FooterLinkTitle>
+                        <FooterLink to="/sign-up">Contact Us</FooterLink>
+                        <FooterLink to="/">Contact</FooterLink>
+                        <FooterLink to="/">Support</FooterLink>
+                        <FooterLink to="/">Sponsorships</FooterLink>
+                    </FooterLinksItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                    
+
+                    <FooterLinksItems>
+                        <FooterLinkTitle>
+                            Social Media
+                        </FooterLinkTitle>
+                        <FooterLink to="/">Instagram</FooterLink>
+                        <FooterLink to="/">Facebook</FooterLink>
+                        <FooterLink to="/">Youtube</FooterLink>
+                        <FooterLink to="/">Twitter</FooterLink> 
+                        <FooterLink to="/">LinkedIn</FooterLink>
+                    </FooterLinksItems>
+                </FooterLinksWrapper>
+            </FooterLinksContainer>
+            <SocialMedia>
+                <SocialMediaWrap>
+                    <SocialLogo to="/">
+                        <SocialIcon />
+                        CAREER SHOCK 
+                    </SocialLogo>
+                    <WebsiteRights>
+                        CODE SHOCK 2021
+                    </WebsiteRights>
+                    <SocialIcons>
+                        <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                            <FaFacebook />
+                        </SocialIconLink>
+                        <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                            <FaInstagram />
+                        </SocialIconLink>
+                        <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                            <FaYoutube />
+                        </SocialIconLink>
+                        <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                            <FaTwitter />
+                        </SocialIconLink>
+                        <SocialIconLink href="/" target="_blank" aria-label="LinkedIn">
+                            <FaLinkedin />
+                        </SocialIconLink>
+                    </SocialIcons>
+                </SocialMediaWrap>
+            </SocialMedia>
+        </FooterContainer>
+    )
+}
+
+export default Footer
