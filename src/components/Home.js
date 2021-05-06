74f4d6512c85887ff4b68b200d8279ff855aba32
BrianReisman <brianreisman@protonmail.com>
2021-05-06 09:35:10 -0400

    remove unused imports and components duplicated in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,8 @@
 import React from 'react'
-// import styled from 'styled-components'
+
 //import components
-// import LoginBtn from './auth/LoginBtn'
-// import LogoutBtn from './auth/LogoutBtn'
-// import SignIn from './auth/SignIn'
 import SearchBox from './SearchBox/SearchBox'
 import Testimonials from './Testimonials'
-import HomePage from '../HomePage/Homepage'
-import GetStarted from './GetStarted/Getstarted'
-import InfoSection from './InfoSection/InfoSection'
-//import { homeObjOne } from '../HomePage/Data'
-
-/*const ComingSoon = styled.h1`
-  color: ${(pr) => pr.theme.primaryColor};
-`
-const Teaser = styled.p`
-  color: ${(pr) => pr.theme.white};
-`*/
 
 const Home = () => {
   return (
@@ -24,16 +10,7 @@ const Home = () => {
       <div className='App'>
         <header className='App-header'>
           <SearchBox />
-          <HomePage />
-          <homeObjOne />
-          <InfoSection />
-          <GetStarted />
         </header>
-      </div>
-      <div className='App'>
-        <footer className='App-footer'>
-          <Footer />
-        </footer>
       </div>
       <Testimonials />
     </>
