88ea3ef72e32aa36cbddfb64517d97c62f659174
zac <Zac Smith>
2021-05-06 21:42:56 -0400

    organize authenticated structure

---
+++ src/components/authenticated/auth/LoginBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginBtn = () => {
+  return (
+    <button onClick={() => console.log('click from LoginBtn.js')}>
+      Log In
+    </button>
+  )
+}
+
+export default LoginBtn
---
+++ src/components/authenticated/auth/LogoutBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogoutBtn = () => {
+  return (
+    <button onClick={() => console.log('click from button in LogoutBtn.js')}>
+      Log Out
+    </button>
+  )
+}
+
+export default LogoutBtn
