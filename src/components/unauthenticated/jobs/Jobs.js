cc1b134cc55dd51b3aedad4a147e2a94e98ec6a2
BrianReisman <brianreisman@protonmail.com>
2021-05-09 20:57:44 -0400

    Jobs section styles to match temp wireframe

--- src/components/unauthenticated/jobs/Jobs.js
+++ src/components/unauthenticated/jobs/Jobs.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Container } from './JobStyles'
+import { Container, Heading, Button } from './JobStyles'
 import JobCard from '../../global/jobs/JobCard'
 
 const Jobs = () => {
   return (
     <Container>
+      <Heading>Jobs</Heading>
       <JobCard />
+      <Button>Start/Apply</Button>
     </Container>
   )
 }
