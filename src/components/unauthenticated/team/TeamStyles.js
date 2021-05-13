b97760c4b706dfe50a09ba35c53908678934ace1
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-05-12 18:44:16 -0600

    WIP - Little Bit of Style

--- src/components/unauthenticated/team/TeamStyles.js
+++ src/components/unauthenticated/team/TeamStyles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const Container = styled.section`
   /* display: flex; */
+  color: ${(props) => props.theme.color.primary};
 `
 
 export const Teaser = styled.p`
@@ -9,8 +10,32 @@ export const Teaser = styled.p`
 `
 export const TeamContainer = styled.div`
   display: flex;
+  flex-flow: row wrap;
+  /* align-items: center; */
+  justify-content: center;
+  /* border: 1px solid blue; */
 `
 
 export const MemberCard = styled.div`
-  width: 20%;
+  /* border: 1px solid red; */
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  margin-bottom: 0, 0, 1%, 0;
+
+  img {
+    width: 50%;
+    clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  :hover {
+    align-items: center;
+    img {
+      clip-path: polygon(0 0, 75% 0, 100% 100%, 25% 100%);
+    }
+  }
 `
