ae8ae397d732b1317110c76f3d183c7ba9c39753
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-05-18 16:45:38 -0600

    switched to global link style, fixed some styles

--- src/components/unauthenticated/team/TeamStyles.js
+++ src/components/unauthenticated/team/TeamStyles.js
@@ -3,11 +3,9 @@ import styled from 'styled-components'
 export const Container = styled.section`
   display: flex;
   flex-flow: column nowrap;
-  /* justify-content: space-evenly; */
   align-items: center;
   color: ${(props) => props.theme.color.primary};
   background: ${(pr) => pr.theme.color.background};
-  border: 1px solid red;
   min-width: 100%;
 `
 
@@ -17,28 +15,28 @@ export const Teaser = styled.p`
 export const TeamContainer = styled.div`
   display: flex;
   flex-flow: row wrap;
-  /* align-items: center; */
   justify-content: center;
-  /* border: 1px solid blue; */
 `
 
 export const MemberCard = styled.div`
-  /* border: 1px solid red; */
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
-  margin-bottom: 0, 0, 1%, 0;
+  margin-bottom: 1%;
 
+  h3 {
+    margin: 1% 0 0 0;
+  }
   img {
     width: 50%;
     clip-path: polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%);
     transition: clip-path 0.25s ease-in-out;
   }
-
   a {
-    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
-
   :hover {
     img {
       clip-path: polygon(0 0, 75% 0, 100% 100%, 25% 100%);
