360e4442f6207a129a8320e4aba046e9ebd7be97
Zac Smith <zrs3141592@gmail.com>
2021-05-18 20:29:41 -0600

    add boxes to dashboard to hold components

--- src/components/authenticated/dashboard/Dashboard.js
+++ src/components/authenticated/dashboard/Dashboard.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Col, Row } from 'react-styled-flexboxgrid'
 import { Container } from './DashboardStyles'
 
 const Dashboard = () => {
   return (
     <Container>
-      <p>Dashboard goes here</p>
+      <Row>
+        <Col style={{ background: 'red' }} xs>
+          one box
+        </Col>
+        <Col style={{ background: 'white' }} xs>
+          two box
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ background: 'blue' }} xs>
+          three box
+        </Col>
+        <Col style={{ background: 'orange' }} xs>
+          four box
+        </Col>
+        <Col style={{ background: 'green' }} xs>
+          five box
+        </Col>
+      </Row>
     </Container>
   )
 }
