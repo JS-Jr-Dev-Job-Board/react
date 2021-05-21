bc8acc8337b122f1bb45b30f9a19c3acc9605e1d
BrianReisman <brianreisman@protonmail.com>
2021-05-20 21:30:37 -0400

    fix weird styling layout for testimonial and job sections on landing page

--- src/components/unauthenticated/jobs/JobStyles.js
+++ src/components/unauthenticated/jobs/JobStyles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  height: ${(props) => props.theme.height.container};
+  display: flex;
+  flex-direction: column;
 `
 
 export const Header = styled.section``
