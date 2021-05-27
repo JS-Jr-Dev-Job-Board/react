e1e8a46a10e4029cb797c246db1f4251a9cfe2e0
unknown <fullstack.melissa@gmail.com>
2021-05-27 10:30:55 -0700

    styles mostly complete. need button

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import Login from './components/unauthenticated/auth_forms/Login'
 import Footer from './components/global/footer/Footer'
 import Dashboard from './components/authenticated/dashboard/Dashboard'
 
+//import Sidebar from './components/global/sidebar/Sidebar'
+
 import darkTheme from './theme/darkTheme'
 import lightTheme from './theme/lightTheme'
 import { Container } from './AppStyles'
--- src/components/global/sidebar/SidebarStyles.js
+++ src/components/global/sidebar/SidebarStyles.js
@@ -1,14 +1,14 @@
 import styled from "styled-components"
-
-
-
+import { Link as LinkS } from "react-scroll"
+import { Link as LinkR } from "react-router-dom"
+import { FaTimes } from "react-icons/fa"
 
 export const SidebarContainer = styled.aside`
   position: fixed;
   z-index: 999;
   width: 100%;
   height: 100%;
-  background: #0d0d0d;
+  background:#d3d3d3;
   display: grid;
   align-items: center;
   top: 0;
@@ -16,4 +16,66 @@ export const SidebarContainer = styled.aside`
   transition: 0.3s ease-in-out;
   opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
   top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+`
+export const CloseIcon = styled(FaTimes)`
+  color: #FC8C09
+`
+export const Icon = styled.div`
+  position: absolute;
+  top: 1.2rem;
+  right: 1.5rem;
+  background: transparent;
+  font-size: 2rem;
+  cursor: pointer;
+  outline: none;
+`
+export const SidebarWrapper = styled.div`
+  color: #000;
+`
+export const SidebarMenu = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(6, 80px);
+  text-align: center;
+  @media screen and (max-width: 480px) {
+    grid-template-rows: repeat(6, 60px);
+  }
+`
+export const SidebarLink = styled(LinkS)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  color: #000;
+  cursor: pointer;
+  &:hover {
+    color: #01bf71;
+    transition: 0.2s ease-in-out;
+  }
+`
+export const SideBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`
+export const SidebarRoute = styled(LinkR)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 16px 64px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  &:hover {
+    color: #010606;
+    background-color: #fff;
+    transition: all 0.2s ease-in-out;
+  }
 `
