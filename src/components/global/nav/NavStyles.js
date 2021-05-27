73175962792b66df587e25173b512490a2f1c12a
unknown <fullstack.melissa@gmail.com>
2021-05-27 09:19:58 -0700

    initial sidebarJs commit

--- src/components/global/nav/NavStyles.js
+++ src/components/global/nav/NavStyles.js
@@ -6,6 +6,7 @@ export const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  
 `
 
 export const NavLinks = styled.div`
---
+++ src/components/global/sidebar/Sidebar.js
@@ -0,0 +1,45 @@
+import React from "react"
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SideBtnWrap,
+  SidebarLink,
+  SidebarRoute,
+  SidebarWrapper,
+  SidebarMenu,
+} from "./SidebarElement"
+
+const sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="home" onClick={toggle}>
+            home
+          </SidebarLink>
+          <SidebarLink to="team" onClick={toggle}>
+            team
+          </SidebarLink>
+          <SidebarLink to="developers" onClick={toggle}>
+            developers
+          </SidebarLink>
+          <SidebarLink to="employers" onClick={toggle}>
+            employers
+          </SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/login">login</SidebarRoute>
+        </SideBtnWrap>
+        <SideBtnWrap>
+          <SidebarRoute to="/register">register</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default sidebar
