c6d0ce7cbbe2387e811779ce756e7b2e6c15448c
unknown <fullstack.melissa@gmail.com>
2021-05-27 09:55:33 -0700

    sidebar container

---
+++ src/components/global/sidebar/SidebarStyles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+
+
+
+export const SidebarContainer = styled.aside`
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: #0d0d0d;
+  display: grid;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  top: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+`
